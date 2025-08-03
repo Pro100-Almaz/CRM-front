a1cd7d911ada93d3e61f487d69ea35e2b0786f7c
Almaz <q19.almaz@gmail.com>
2025-08-03 07:27:44 +0500

    removing prime vue from main.ts

--- src/main.ts
+++ src/main.ts
@@ -16,40 +16,14 @@ import { initApexCharts } from "@/core/plugins/apexcharts";
 import { initInlineSvg } from "@/core/plugins/inline-svg";
 import { initVeeValidate } from "@/core/plugins/vee-validate";
 import { initKtIcon } from "@/core/plugins/keenthemes";
-import Aura from "@primeuix/themes/aura";
-import { definePreset } from "@primeuix/themes";
-import PrimeVue from 'primevue/config';
 import 'primeicons/primeicons.css';
 import "@/core/plugins/prismjs";
 
 const app = createApp(App);
 
-const MyPreset = definePreset(Aura, {
-  semantic: {
-    primary: {
-      50: '{blue.50}',
-      100: '{blue.100}',
-      200: '{blue.200}',
-      300: '{blue.300}',
-      400: '{blue.400}',
-      500: '{blue.500}',
-      600: '{blue.600}',
-      700: '{blue.700}',
-      800: '{blue.800}',
-      900: '{blue.900}',
-      950: '{blue.950}'
-    }
-  }
-});
-
 app.use(createPinia());
 app.use(router);
 app.use(ElementPlus);
-app.use(PrimeVue, {
-  theme: {
-    preset: MyPreset
-  }
-});
 
 
 ApiService.init(app);
